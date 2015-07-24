715a8895ef1986367e565b761109364f8c6f4f2c
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-24 16:47:26 +0300

    немного поправил

--- setup.py
+++ setup.py
@@ -2,19 +2,27 @@
 
 from distutils.core import setup, Extension
 
+import shutil
+
+shutil.copyfile('README.md', 'README')
+
 setup(name='Silly Crossbow',
       version='1.0',
       description="""
       Simple SWIG + distutil example
       example implements cropping transparent image borders
       """,
+      long_description=open('README.md').read(),
       author='Shnaider Pavel',
       author_email='shnaiderpasha@gmail.com',
       url='https://github.com/Ingener74/Silly-Crossbow',
       ext_modules=[
           Extension(
-              "_SillyCrossbow",
-              ["SillyCrossbow.i", "silly-crossbow.cpp"],
-              swig_opts=['-c++'])
-      ]
+              "_SillyCrossbow",[
+                  "SillyCrossbow.i",
+                  "silly-crossbow.cpp"],
+              swig_opts=['-c++']
+          )
+      ],
+      scripts=['SillyCrossbow.py']
       )
