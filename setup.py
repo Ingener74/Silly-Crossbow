f6ad753892517920e6b8a8d3e7f9d52753e5b3df
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-29 14:53:50 +0300

    поправил глупую ошибку

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class Installation(install_lib):
 shutil.copyfile('README.md', 'README')
 
 setup(name='SillyCrossbow',
-      version='1.0.5',
+      version='1.0.6',
       description="""
 Simple SWIG + distutil example
 example implements cropping transparent image borders
